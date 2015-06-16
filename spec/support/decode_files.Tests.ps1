d8673e9f08e58ef1d0a360ed4afa689a0c396f15
Steven Murawski <steven.murawski@gmail.com>
2015-06-16 16:24:14 +0000

    Single files failed when parent directory not present.

--- spec/support/decode_files.Tests.ps1
+++ spec/support/decode_files.Tests.ps1
@@ -61,6 +61,33 @@ describe 'Decode-Base64File' {
     }
     rm $EncodedSource, $DecodedDestination
   }
+
+  context 'when the parent folder structure does not exist' {
+    $Content = 'QHsKICAiJGVudjpURU1QXGI2NC1mMWFlNGMyNzgwMWQ0NDMyNjliMjAyZTEyYWYyMDdkYS50eHQiID0gQHsKICAgICJkc3QiID0gIiRlbnY6VEVNUFxraXRjaGVuIgogICAgInRtcHppcCIgPSAiJGVudjpURU1QXHRtcHppcC1mMWFlNGMyNzgwMWQ0NDMyNjliMjAyZTEyYWYyMDdkYS56aXAiCiAgfQogICIkZW52OlRFTVBcYjY0LThjYzM5NDViZTA0NWZhNGY4YjAyZmFmM2I3ZDRjZGJkLnR4dCIgPSBAewogICAgImRzdCIgPSAiJGVudjpURU1QXGtpdGNoZW4iCiAgICAidG1wemlwIiA9ICIkZW52OlRFTVBcdG1wemlwLThjYzM5NDViZTA0NWZhNGY4YjAyZmFmM2I3ZDRjZGJkLnppcCIKICB9Cn0= '
+    $EncodedSource = "$env:temp\encoded.txt"
+    if (test-path $env:temp\test) {rm -recurse -force $env:temp\test}
+    $DecodedDestination = "$env:temp\test\decoded.txt"
+    Set-Content $EncodedSource -value $content
+    Decode-Base64File $EncodedSource $DecodedDestination
+    $DecodedContent  = gc $DecodedDestination | out-string
+    $hash = invoke-expression $DecodedContent
+
+    it 'has two keys' {
+      $hash.keys.count | should be 2
+    }
+    it 'has a key that matches $env:TEMP\b64-f1ae4c27801d443269b202e12af207da.txt' {
+      $hash.keys |
+        where {$_ -like "$env:TEMP\b64-f1ae4c27801d443269b202e12af207da.txt"} |
+        should not be $null
+    }
+    it 'has keys with values including "dst" and "tmpzip"' {
+       $hash.keys |
+        where { -not ( ($hash[$_].keys -contains 'dst') -and
+          ($hash[$_].keys -contains 'tmpzip') ) } |
+        should be $null
+    }
+    rm $EncodedSource, $DecodedDestination
+  }
 }
 
 if ($PSVersionTable.PSVersion.Major -ge 4) {
--- support/decode_files.ps1
+++ support/decode_files.ps1
@@ -1,12 +1,12 @@
 trap {$e = $_.Exception; $e.InvocationInfo.ScriptName; do {$e.Message; $e = $e.InnerException} while ($e); break;}
-$progresspreference = $warningpreference = 'SilentlyContinue'
-function Decode-Base64File($src, $dst) {sc -force -Encoding Byte -Path $dst -Value ([Convert]::FromBase64String([IO.File]::ReadAllLines($src)))}
-function Copy-Stream($src, $dst) { $b = New-Object Byte[] 4096; while (($i = $src.Read($b, 0, $b.Length)) -ne 0) { $dst.Write($b, 0, $i) } }
+$progresspreference = 'SilentlyContinue'
+function Decode-Base64File($src, $dst) {folder (split-path $dst);sc -force -Encoding Byte -Path $dst -Value ([Convert]::FromBase64String([IO.File]::ReadAllLines($src)))}
+function Copy-Stream($src, $dst) {$b = New-Object Byte[] 4096; while (($i = $src.Read($b, 0, $b.Length)) -ne 0) { $dst.Write($b, 0, $i) } }
 function Resolve-ProviderPath{ $input | % {if ($_){(Resolve-Path $_).ProviderPath} else{$null}} }
 function Get-FrameworkVersion { "Full", "Client" | % {([version](gp "HKLM:\Software\Microsoft\NET Framework Setup\NDP\v4\$_").Version)} | select -first 1}
 function Test-NETStack($Version){ Get-FrameworkVersion -ge $Version }
 function Test-IOCompression {($PSVersionTable.PSVersion.Major -ge 3) -and (Test-NETStack '4.5')}
-function directory($path){ $path | ? {-not (test-path $_)} | % {$null = mkdir $_}}
+function folder($path){ $path | ? {-not (test-path $_)} | % {$null = mkdir $_}}
 function disposable($o){($o -is [IDisposable]) -and (($o | gm | %{$_.name}) -contains 'Dispose')}
 function use($obj, [scriptblock]$sb){try {& $sb} catch [exception]{throw $_} finally {if (disposable $obj) {$obj.Dispose()}} }
 set-alias RPP -Value Resolve-ProviderPath
@@ -44,7 +44,7 @@ Function Invoke-Input($in) {
 
 Function Unzip-File($src, $dst) {
   $unpack = $src -replace '\.zip'
-  directory $unpack, $dst
+  folder $unpack, $dst
   if (Test-IOCompression) {Add-Type -AN System.IO.Compression.FileSystem; [IO.Compression.ZipFile]::ExtractToDirectory($src, $unpack)}
   else {Try {$s = New-Object -ComObject Shell.Application; ($s.NameSpace($unpack)).CopyHere(($s.NameSpace($src)).Items(), 0x610)} Finally {[void][Runtime.Interopservices.Marshal]::ReleaseComObject($s)}}
   dir $unpack | cp -dest "$dst/" -force -recurse
